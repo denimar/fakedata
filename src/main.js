15d59510e997a8a171cdb368e30e1233184f1654
Denimar de Moraes <denimar@gmail.com>
2018-01-02 11:20:47 -0200

    first commit

--- src/main.js
+++ src/main.js
@@ -4,15 +4,19 @@ const fs = require('fs');
 const jsonsRoutesStr = fs.readFileSync('./src/jsons-routes.json');
 const jsonsRoutes = JSON.parse(jsonsRoutesStr);
 
-for (let i = 0 ; i < jsonsRoutes.length ; i++) {
-  let jsonRoute = jsonsRoutes[i];
-  app.get(jsonRoute.route, function (req, res) {
-    let rawdata = fs.readFileSync('./data/' + jsonRoute.json);
-    let student = JSON.parse(rawdata);
-    res.send(student);
-  });
-}
+// for (let i = 0 ; i < jsonsRoutes.length ; i++) {
+//   let jsonRoute = jsonsRoutes[i];
+//   app.get(jsonRoute.route, function (req, res) {
+//     let rawdata = fs.readFileSync('./data/' + jsonRoute.json);
+//     let student = JSON.parse(rawdata);
+//     res.send(student);
+//   });
+// }
 
-app.listen(function () {
-  console.log('Example app listening!');
+app.get('/', function(req, res) {
+  res.send('testes aqui');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
