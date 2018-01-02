f972749100c53287180df68ba5c443a8ebb01e64
Denimar de Moraes <denimar@gmail.com>
2018-01-02 13:12:23 -0200

    first commit

--- src/main.js
+++ src/main.js
@@ -4,19 +4,19 @@ const fs = require('fs');
 const jsonsRoutesStr = fs.readFileSync('./src/jsons-routes.json');
 const jsonsRoutes = JSON.parse(jsonsRoutesStr);
 
-// for (let i = 0 ; i < jsonsRoutes.length ; i++) {
-//   let jsonRoute = jsonsRoutes[i];
-//   app.get(jsonRoute.route, function (req, res) {
-//     let rawdata = fs.readFileSync('./data/' + jsonRoute.json);
-//     let student = JSON.parse(rawdata);
-//     res.send(student);
-//   });
-// }
-app.use(express.static('./public'));
+for (let i = 0 ; i < jsonsRoutes.length ; i++) {
+  let jsonRoute = jsonsRoutes[i];
+  app.get(jsonRoute.route, function (req, res) {
+    let rawdata = fs.readFileSync('./data/' + jsonRoute.json);
+    let student = JSON.parse(rawdata);
+    res.send(student);
+  });
+}
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.set('content-type', 'text/html')
-  res.send('testes aqui');
+  res.send('Fake Data Here');
 });
 
 
