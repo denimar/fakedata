b625b44cc6db93551f355f00041a8c380cb44bf7
Denimar de Moraes <denimar@gmail.com>
2018-01-03 00:29:14 -0200

    fixing...

--- src/get-data.js
+++ src/get-data.js
@@ -16,9 +16,27 @@ class DataUtils {
       responseData = jsonData.slice(start, limit);
     }
 
+    // console.log('--------------------');
+    // console.log('--------------------');
+    // console.log(req.query);
+    // console.log('--------------------');
+    // console.log('--------------------');
+
+
     let jsonResponse = null;
     if (req.url.indexOf('/tree/') !== -1) {
-      jsonResponse = responseData;
+
+      let lazyLoad = JSON.parse(req.query.lazyLoad || false);
+      if (lazyLoad) {
+        let item = req.query.item;
+        if (item) {
+          jsonResponse = _getChildren(responseData, JSON.parse(item));
+        } else {
+          //throw new Error('You have to set "item" param when lazy load.');
+        }
+      } else {
+        jsonResponse = responseData;
+      }
     } else {
       jsonResponse = {
         success: true,
@@ -40,5 +58,52 @@ class DataUtils {
   }
 
 }
+
+function _findItem(data, item) {
+  if (data) {
+    if (item.root) {
+      return {children: data};
+    } else {
+      if (data) {
+        for (let i = 0 ; i < data.length ; i++) {
+          let dataItem = data[i];
+          if (dataItem.id === item.id) {
+            return dataItem;
+          } else {
+            let foundItem = _findItem(dataItem.children, item)
+            if (foundItem) {
+              return foundItem;
+            }
+          }
+        }
+      }
+    }
+  }
+  return null;
+}
+
+function _getChildren(allData, item) {
+  let foundItem = _findItem(allData, item);
+
+  let childrenToReturn = [];
+  let children = foundItem.children || [];
+
+  // console.log('++++++++++++++++++++++');
+  // console.log('++++++++++++++++++++++');
+  // console.log(children);
+  // console.log('++++++++++++++++++++++');
+  // console.log('++++++++++++++++++++++');
+
+  for (let i = 0 ; i < children.length ; i++) {
+    let child = children[i];
+    childrenToReturn.push({
+      id: child.id,
+      text: child.text,
+      isLeaf: child.isLeaf
+    });
+  }
+  return childrenToReturn;
+}
+
 
 module.exports = DataUtils;
